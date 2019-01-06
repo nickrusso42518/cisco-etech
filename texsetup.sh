111e604ff27800e057acee84c53c3b324360e5ee
Nicholas Russo <njrusmc@gmail.com>
2019-01-05 21:06:10 -0500

    unicase and texsetup fixes

--- texsetup.sh
+++ texsetup.sh
@@ -1,8 +1,29 @@
-# Download the newest version of TexLive installer
+# Packages required above and beyond the ones include with "texlive-basic".
+# Everything after "minted" is a dependency of it, not directly
+# included by any tex source files in this project.
+TL_PKGS=(
+    booktabs
+    caption
+    chktex
+    minted
+    fvextra
+    etoolbox
+    fancyvrb
+    xcolor
+    upquote
+    lineno
+    ifplatform
+    xstring
+    framed
+    float
+)
+
+# Download the newest version of TexLive installer for Linux
 wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 
 # Untar the file without the top level directory, since
-# it has an obxiously variable name
+# it has an variable name, such as "install-tl-20190105/" and
+# "--strip-components" avoids the  directory creation entirely
 tar -xzvf install-tl-unx.tar.gz --strip-components 1
 
 # Perform the basic installation based on the premade profile
@@ -10,11 +31,9 @@ tar -xzvf install-tl-unx.tar.gz --strip-components 1
 
 # root needs its PATH updated for tlmgr to work
 PATH=$PATH:/usr/local/texlive/2018/bin/x86_64-linux
+
+# Quickly test to see if the PATH was properly updated
 tlmgr --version
 
-# Note: Everything after "minted" is a dependency of it, not directly
-# included by any tex source files in this project.
-# tar --strip-components avoids the install-tl-(date) directory creation
-tlmgr install booktabs caption chktex
-tlmgr install minted fvextra etoolbox fancyvrb xcolor upquote
-tlmgr install lineno ifplatform xstring framed float
+# Install all of the packages at once
+tlmgr install ${TL_PKGS[@]}
